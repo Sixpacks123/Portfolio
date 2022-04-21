efc7aa90ceb0436e9f8ca91b036b316735a8c36e
Heurtault Aubin <aub.heurtault@gmail.com>
2022-04-21 15:36:38 +0200

    ajout nouvelle page ctf

---
+++ pages/ctf.vue
@@ -0,0 +1,22 @@
+<script>
+import feather from "feather-icons";
+
+export default {
+  name: "CTF",
+  scrollToTop: true,
+  mounted() {
+    feather.replace();
+  },
+  updated() {
+    feather.replace();
+  },
+};
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style scoped></style>
